3035df452d7da853fe59a14ed1470f82a2664889
k1s3n <christoffer@rehnmark.se>
2024-02-29 16:37:49 +0100

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+// Use Express - a web server module
+import express from 'express';
+
+// The port to run the server on
+const port = 80;
+
+// Create a new web server
+// store in the variable app
+const app = express();
+
+// Serve all files in the www folder
+// to the web browser
+app.use(express.static('www'));
+
+// Start the web server
+app.listen(port, () => console.log('Running on http://localhost:' + port));
