12ec09ed3ebacbf7b723dd35a2242d06600c90f6
k1s3n <christoffer@rehnmark.se>
2024-03-02 23:06:50 +0100

    CSS är crazt

--- www/script.js
+++ www/script.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Hitta linjerna
+    var line1 = document.querySelector(".line1");
+    var line2 = document.querySelector(".line2");
+  
+    // Använd en timeout för att starta animationen för linje 2 efter en viss fördröjning
+    setTimeout(function() {
+      line2.style.animationDelay = "2s"; // Fördröjning för linje 2
+      line2.style.animationDuration = "2s"; // Varaktighet för linje 2
+    }, 2000); // Vänta 2 sekunder innan linje 2 börjar animera
+  });
+  
+
+document.addEventListener("DOMContentLoaded", function() {
     var links = document.querySelectorAll("header a");
   
     links.forEach(function(link) {
@@ -16,3 +29,4 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
