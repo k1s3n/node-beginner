b94ccfea05a64263e9c439c097eab5df22bdd4ad
k1s3n <christoffer@rehnmark.se>
2024-04-14 09:18:16 +0200

    hej

--- www/main.js
+++ www/main.js
@@ -157,33 +157,3 @@ window.addEventListener('popstate', () => showView());
 
 // Show the first view after hard page load/reload
 showView();
-
-
-$(document).ready(function() {
-  var nav = $('.menu-container');
-  var main = $('main');
-  var navOffset = nav.offset().top;
-  var scrollThreshold = 100; // Adjust this value according to your needs
-
-  $(window).scroll(function() {
-    var scrollTop = $(window).scrollTop();
-
-    // Check if the user has scrolled beyond the threshold
-    if (scrollTop > scrollThreshold) {
-      // Add the 'fixed' class to make the menu container sticky
-      nav.addClass('sticky');
-      // Remove top margin when menu container becomes sticky
-      nav.css('margin-top', '0');
-      var parentWidth = nav.parent().width();
-            var navWidth = nav.outerWidth();
-            var leftOffset = (parentWidth - navWidth) / 2;
-            nav.css('left', leftOffset + 'px');
-    } else {
-      // Remove the 'fixed' class if the user scrolls back above the threshold
-      nav.removeClass('sticky');
-      // Restore top margin when menu container is not sticky
-      nav.css('margin-top', ''); // Reset margin to default
-      nav.css('left', '');
-    }
-  });
-});
